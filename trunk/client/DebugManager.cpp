b7570f2388169a110f998fa014cc222bb7855904
bigmuscle <bigmuscle@5fb55d53-692c-0410-a46a-e90ab66e00ee>
2004-05-04 15:32:30 +0000

    Removed

--- trunk/client/DebugManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdinc.h"
-#include "DebugManager.h"
-
-
-DebugManager::DebugManager() {}
-void DebugManager::SendDebugMessage(const string &mess) {
-	fire(DebugManagerListener::DEBUG_MESSAGE, mess);
-}
-DebugManager::~DebugManager() {}
