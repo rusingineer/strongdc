515b6d624d2803b6bb93a35d46a0fef7b13028bd
bigmuscle <bigmuscle@5fb55d53-692c-0410-a46a-e90ab66e00ee>
2004-09-21 18:34:45 +0000

    patches

---
+++ trunk/windows/PopupDlg.h
@@ -0,0 +1,107 @@
+#ifndef POPUPWND_H
+#define POPUPWND_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "stdafx.h"
+#include "../client/DCPlusPlus.h"
+#include "Resource.h"
+#include "WinUtil.h"
+
+class PopupWnd : public CWindowImpl<PopupWnd, CWindow>
+{
+public:
+	DECLARE_WND_CLASS("Popup");
+
+	BEGIN_MSG_MAP(PopupWnd)
+		MESSAGE_HANDLER(WM_CREATE, onCreate)
+		MESSAGE_HANDLER(WM_CLOSE, onClose)
+		MESSAGE_HANDLER(WM_CTLCOLORSTATIC, onCtlColor)
+		MESSAGE_HANDLER(WM_LBUTTONDOWN, onLButtonDown)
+	END_MSG_MAP()
+
+	PopupWnd(const string& aMsg, const string& aTitle, CRect rc): visible(GET_TICK()) {
+		if(int(aMsg.length()) > 256){
+			msg = aMsg.substr(0, 253);
+			msg += "...";
+		}
+		else
+			msg = aMsg;
+			title = aTitle;
+
+		Create(NULL, rc, NULL, WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, WS_EX_TOOLWINDOW );
+
+		WinUtil::decodeFont(SETTING(TEXT_FONT)/*SETTING(POPUP_FONT)*/, logFont);
+		font = ::CreateFontIndirect(&logFont);
+
+	}
+
+	~PopupWnd(){
+		DeleteObject(font);
+	}
+
+	LRESULT onLButtonDown(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/){
+		CRect rc;
+		GetWindowRect(rc);
+		::PostMessage(WinUtil::mainWnd, WM_SPEAKER, WM_CLOSE, (LPARAM)rc.bottom);
+		return 0;
+	}
+
+	LRESULT onCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled){
+		::SetClassLongPtr(m_hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)::GetSysColorBrush(COLOR_INFOTEXT));
+		CRect rc;
+		GetClientRect(rc);
+
+		rc.top += 1;
+		rc.left += 1;
+		rc.right -= 1;
+		rc.bottom /= 3;
+
+		label.Create(m_hWnd, rc, NULL, WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN |
+			SS_CENTER | SS_NOPREFIX);
+
+		rc.top += rc.bottom - 1;
+		rc.bottom *= 3;
+
+		label1.Create(m_hWnd, rc, NULL, WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN |
+			SS_CENTER | SS_NOPREFIX);
+
+		label.SetFont(WinUtil::boldFont);
+		label.SetWindowText(title.c_str());
+		label1.SetFont(WinUtil::font);
+		label1.SetWindowText(msg.c_str());
+
+
+		bHandled = false;
+		return 1;
+	}
+
+	LRESULT onClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled){
+		label.DestroyWindow();
+		label.Detach();
+		DestroyWindow();
+
+		bHandled = false;
+		return 1;
+	}
+
+	LRESULT onCtlColor(UINT /*uMsg*/, WPARAM wParam, LPARAM lParam, BOOL& /*bHandled*/) {
+		HDC hDC = (HDC)wParam;
+				::SetBkColor(hDC, ::GetSysColor(COLOR_INFOBK));
+				::SetTextColor(hDC, ::GetSysColor(COLOR_INFOTEXT));
+		return (LRESULT)::GetSysColorBrush(COLOR_INFOBK);
+	}
+
+	u_int32_t visible;
+
+private:
+	string  msg, title;
+	CStatic label, label1;
+	LOGFONT logFont;
+	HFONT   font;
+};
+
+
+#endif
---
+++ trunk/windows/PopupManager.cpp
@@ -0,0 +1,157 @@
+#include "stdafx.h"
+#include "../client/DCPlusPlus.h"
+#include "../client/SettingsManager.h"
+#include "../client/Util.h"
+
+#include "WinUtil.h"
+#include "PopupManager.h"
+#include "MainFrm.h"
+
+
+PopupManager* Singleton< PopupManager >::instance = NULL;
+
+void PopupManager::Show(const string &aMsg, const string &aTitle, int Icon) {
+	if(!activated)
+		return;
+
+	if(SETTING(POPUP_TYPE) == 0) {
+
+		NOTIFYICONDATA m_nid;
+		m_nid.cbSize = sizeof(NOTIFYICONDATA);
+		m_nid.hWnd = MainFrame::getMainFrame()->m_hWnd;
+		m_nid.uID = 0;
+		m_nid.uFlags = NIF_INFO;
+		m_nid.uTimeout = 5000;
+		m_nid.dwInfoFlags = Icon;
+		strcpy(m_nid.szInfo, aMsg.c_str());
+		strcpy(m_nid.szInfoTitle, aTitle.c_str());
+		Shell_NotifyIcon(NIM_MODIFY, &m_nid);
+		return;
+
+	}
+
+/*	if (!Util::getAway() && BOOLSETTING(POPUP_AWAY)) {
+		return;
+	}
+
+	if(!minimized && BOOLSETTING(POPUP_MINIMIZED)) 
+		return;*/
+	
+	
+	CRect rcDesktop;
+	
+	//get desktop rect so we know where to place the popup
+	::SystemParametersInfo(SPI_GETWORKAREA,0,&rcDesktop,0);
+	
+	int screenHeight = rcDesktop.bottom;
+	int screenWidth = rcDesktop.right;
+
+	Lock l(cs);
+
+	//if we have popups all the way up to the top of the screen do not create a new one
+	if( (offset + height) > screenHeight)
+		return;
+	
+	//get the handle of the window that has focus
+	HWND gotFocus = ::SetFocus(WinUtil::mainWnd);
+	
+	//compute the window position
+	CRect rc(screenWidth - width , screenHeight - height - offset, screenWidth, screenHeight - offset);
+	
+	//Create a new popup
+	PopupWnd *p = new PopupWnd(aMsg, aTitle, rc);
+			
+	//move the window to the top of the z-order and display it
+	p->SetWindowPos(HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+	p->ShowWindow(SW_SHOWNA);
+	
+		
+	//restore focus to window
+	::SetFocus(gotFocus);
+
+	
+	
+	//increase offset so we know where to place the next popup
+	offset = offset + height;
+
+	popups.push_back(p);
+}
+
+void PopupManager::on(TimerManagerListener::Second /*type*/, u_int32_t tick) {
+//	if(!BOOLSETTING(REMOVE_POPUPS))
+//		return;
+
+	Lock l(cs);
+
+	//we got nothing to do here
+	if(popups.empty()) {
+		return;
+	}
+
+	//check all popups and see if we need to remove anyone
+	PopupList::iterator i = popups.begin();
+	for(; i != popups.end(); ++i) {
+
+		if((*i)->visible + /*SETTING(POPUP_TIMEOUT)*/5 * 1000 < tick) {
+			//okay remove the first popup
+			Remove();
+
+			//if list is empty there is nothing more to do
+			if(popups.empty())
+				return;
+				
+			//start over from the beginning
+			i = popups.begin();
+		}
+	}
+}
+
+void PopupManager::Remove(int pos) {
+	Lock l(cs);
+
+	CRect rcDesktop;
+
+	//get desktop rect so we know where to place the popup
+	::SystemParametersInfo(SPI_GETWORKAREA,0,&rcDesktop,0);
+	if(pos == 0)
+		pos = rcDesktop.bottom;
+
+	//find the correct window
+	int end = (rcDesktop.bottom - pos) / height;
+	PopupList::iterator i = popups.begin();
+	for(int j = 0; j < end; ++j, ++i);
+	
+	//remove the window from the list
+	PopupWnd *p = (*i);
+	popups.erase(i);
+	
+	//close the window and delete it
+	if(p == NULL){
+		return;
+	}
+	
+	p->SendMessage(WM_CLOSE, 0, 0);
+	delete p;
+	p = NULL;
+	
+	    
+	//set offset one window position lower
+	dcassert(offset > 0);
+	offset = offset - height;
+
+	//nothing to do
+	if(popups.empty())
+		return;
+
+	CRect rc;
+
+	//move down all windows
+	for(i = popups.begin(); i != popups.end(); ++i) {
+		(*i)->GetWindowRect(rc);
+		if(rc.bottom <= pos){
+			rc.top += height;
+			rc.bottom += height;
+			(*i)->MoveWindow(rc);
+		}
+	}
+}
