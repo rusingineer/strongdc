a463d0253adc73f7c121e8b48ed5a726f0d19373
BigMuscle <BigMuscle@5fb55d53-692c-0410-a46a-e90ab66e00ee>
2008-07-06 20:14:13 +0000

    added missing files

--- trunk/client/version.h
+++ trunk/client/version.h
@@ -23,7 +23,7 @@
 #define DCVERSIONSTRING "0.707"
 #define VERSION_URL "http://strongdc.sourceforge.net/download/version.xml"
 
-#define SVNVERSION "svn399"
+#define SVNVERSION "svn400"
 
 /* Update the .rc file as well... */
 
