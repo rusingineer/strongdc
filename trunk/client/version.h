72659289a006bba274803ae25f0a7aa10d37d403
BigMuscle <BigMuscle@5fb55d53-692c-0410-a46a-e90ab66e00ee>
2008-07-15 20:53:00 +0000

    fixed fix for transferview :-)

--- trunk/client/version.h
+++ trunk/client/version.h
@@ -23,7 +23,7 @@
 #define DCVERSIONSTRING "0.707"
 #define VERSION_URL "http://strongdc.sourceforge.net/download/version.xml"
 
-#define SVNVERSION "svn408"
+//#define SVNVERSION "svn409"
 
 /* Update the .rc file as well... */
 
